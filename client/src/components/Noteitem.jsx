11ad94480afdf9fee31f82d597232b603d34f52b
Mohammed Armaan <armaan0243@gmail.com>
2024-06-19 23:12:44 +0530

    Noteitem styling

--- client/src/components/Noteitem.jsx
+++ client/src/components/Noteitem.jsx
@@ -9,7 +9,7 @@ const Noteitem = (props) => {
 
   return (
     <>
-      <div className="col-md-3">
+      {/* <div className="col-md-3">
         <div className="card my-3">
           <div className="card-body">
             <h5 className="card-title">{note.title}</h5>
@@ -38,6 +38,40 @@ const Noteitem = (props) => {
             >
               <i className="fa-solid fa-pen-to-square"></i>
             </a>
+          </div>
+        </div>
+      </div> */}
+      <div className="col-md-3">
+        <div className="card my-3 shadow-sm">
+          <div className="card-body">
+            <h5 className="card-title text-truncate">{note.title}</h5>
+            <p className="card-text text-muted">{note.description}</p>
+            <p className="card-text">
+              <span className="badge bg-secondary">{note.tag}</span>
+            </p>
+            <div className="d-flex justify-content-between">
+              <a
+                href="#"
+                className="btn btn-danger"
+                onClick={() => {
+                  deleteNote(note._id);
+                  showAlert("Successfully deleted note", "success");
+                }}
+                title="Delete"
+              >
+                <i className="fa-solid fa-trash"></i>
+              </a>
+              <a
+                href="#"
+                className="btn btn-primary"
+                onClick={() => {
+                  updatenote(note);
+                }}
+                title="Edit"
+              >
+                <i className="fa-solid fa-pen-to-square"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
