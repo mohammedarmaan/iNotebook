93a0385273a68cffdf6d18f2780a344c2dacebc3
Mohammed Armaan <armaan0243@gmail.com>
2024-06-19 23:13:25 +0530

    noteitem styling

--- client/src/components/Noteitem.jsx
+++ client/src/components/Noteitem.jsx
@@ -9,38 +9,7 @@ const Noteitem = (props) => {
 
   return (
     <>
-      {/* <div className="col-md-3">
-        <div className="card my-3">
-          <div className="card-body">
-            <h5 className="card-title">{note.title}</h5>
-            <p className="card-text">
-              {note.description}
-            </p>
-            <p className="card-text">
-              {note.tag}
-            </p>
-            <a
-              href="#"
-              className="btn btn-danger"
-              onClick={() => {
-                deleteNote(note._id);
-                showAlert("Successfully deleted note", "success");
-              }}
-            >
-              <i className="fa-solid fa-trash"></i>
-            </a>
-            <a
-              href="#"
-              className="btn btn-primary mx-3 "
-              onClick={() => {
-                updatenote(note);
-              }}
-            >
-              <i className="fa-solid fa-pen-to-square"></i>
-            </a>
-          </div>
-        </div>
-      </div> */}
+      
       <div className="col-md-3">
         <div className="card my-3 shadow-sm">
           <div className="card-body">
